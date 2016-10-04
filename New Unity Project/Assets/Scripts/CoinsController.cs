77d83a507b0f112f325a3ecf6adf9229309c38ad
Tomas Colloca Bargallo <tomascllc94@gmail.com>
2016-10-03 23:50:45 -0300

    coin spawner

---
+++ New Unity Project/Assets/Scripts/CoinsController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinsController : MonoBehaviour {
+
+	public float spawnFrequency = 3;
+	public GameObjectsPooler coinsPooler; 
+
+	private float time = 0f;
+	private float lastCreationTime = 0;
+	private string boardTag = "Board";
+	private float boardWidth;
+	private float boardHeight;
+
+	// Use this for initialization
+	void Start () {
+		GameObject board = GameObject.FindGameObjectWithTag (boardTag);
+		Bounds boardBounds = board.GetComponent<MeshFilter> ().mesh.bounds;
+		boardWidth = board.transform.localScale.x * boardBounds.size.x * 0.5f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		lastCreationTime += Time.deltaTime;
+		if ((int)time % spawnFrequency == 0 && lastCreationTime > spawnFrequency) {
+			lastCreationTime = 0;
+			GameObject coin = coinsPooler.getObject ();
+			if (coin == null) return;
+			float yPos = +boardWidth;
+			float xPos = +boardWidth;
+			float zPos = 2f;
+			coin.transform.position = new Vector3(xPos, yPos, zPos);
+			coin.SetActive (true);
+		}
+	}
+}
