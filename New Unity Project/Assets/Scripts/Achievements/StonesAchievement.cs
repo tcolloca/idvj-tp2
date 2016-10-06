32ca40f88aa18c3e9aa6224a0964d4e1d4b60f59
Tomas Colloca Bargallo <tomascllc94@gmail.com>
2016-10-05 21:59:55 -0300

    rock it achievement fixed

--- New Unity Project/Assets/Scripts/Achievements/StonesAchievement.cs
+++ New Unity Project/Assets/Scripts/Achievements/StonesAchievement.cs
@@ -4,18 +4,18 @@ using System.Collections;
 public class StonesAchievement : Achievement {
 
 	private static readonly int ID = Achievement.nextId();
-	private static readonly int totalEnemies = 300;
+	private static readonly int enemies = 300;
 	private static readonly Sprite achieved = SpriteRepository.Get("stone");  
 	private static readonly Sprite notAchieved = SpriteRepository.Get("mysterious_ball");  
 
 	public StonesAchievement() : base (achieved, notAchieved, "Rock It!", 
 		"Show them that you are way better than them.", 
-		string.Format("Make {0} enemies fall into the abyss in one game.", totalEnemies)) {
+		string.Format("Make {0} enemies fall into the abyss in one game.", enemies)) {
 		id = ID;
 	}
 
 	public override bool evaluate (GeneralStats generalStats) {
-		if (generalStats.totalEnemiesDefeated >= totalEnemies) {
+		if (generalStats.lastGameStats.enemiesDefeated >= enemies) {
 			return true;
 		}
 		return false;
